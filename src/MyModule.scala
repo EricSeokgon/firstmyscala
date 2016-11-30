06bb53f525b23de2e68a92d6703ba92f5d89d0f1
hadesyi <hadesleeq@gmail.com>
2016-11-30 15:18:00 +0900

    피보나치함수

--- src/MyModule.scala
+++ src/MyModule.scala
@@ -12,11 +12,29 @@ object MyModule {
     if (n < 0) -n
     else n
 
+  def factorial(n: Int): Int = {
+    def go(n: Int, acc: Int): Int =
+      if (n <= 0) acc
+      else go(n - 1, n * acc)
+
+    go(n, 1)
+  }
+
   private def formatAbs(x: Int) = {
     val msg = "The absolute value of %d is %d"
     msg.format(x, abs(x))
   }
 
+  private def formatFactorial(n: Int) = {
+    val msg = "The factorial of %d is %d"
+    msg.format(n, factorial(n))
+  }
+
   def main(args: Array[String]): Unit =
     println(formatAbs(-35))
+
+    println(formatFactorial(5))
 }
+
+
+
