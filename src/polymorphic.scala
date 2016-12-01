553556ee5ccdb8942ed209f5da24034284f6e771
hadesyi <hadesleeq@gmail.com>
2016-12-01 17:19:22 +0900

    다형적 함수의 예

--- src/polymorphic.scala
+++ src/polymorphic.scala
@@ -18,4 +18,23 @@ object polymorphic {
     loop(0)
   }
 
+  def findFirst[A](as: Array[A], p: A => Boolean): Int = {
+    @annotation.tailrec
+    def loop(n: Int): Int =
+      if (n >= as.length) -1
+      else if (p(as(n))) n
+      else loop(n + 1)
+
+    loop(0)
+  }
+
+  def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
+    @annotation.tailrec
+    def loop(n: Int): Int =
+      if (n >= as.length) -1
+      else if (ordered(as(n))) n
+      else loop(n + 1)
+
+    loop(0)
+  }
 }
