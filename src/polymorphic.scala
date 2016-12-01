99d831da94301a64bed36831263a499be5361a5f
hadesyi <hadesleeq@gmail.com>
2016-12-01 17:49:17 +0900

    함수 합성

--- src/polymorphic.scala
+++ src/polymorphic.scala
@@ -41,5 +41,10 @@ object polymorphic {
   def curry[A, B, C](f: (A, B) => C): A => (B => C) =
     a => b => f(a, b)
 
+  def uncurry[A, B, C](f: A => B => C): (A, B) => C =
+    (a, b) => f(a)(b)
+
+  def compose[A, B, C](f: B => C, g: A => B): A => C =
+    a => f(g(a))
 
 }
